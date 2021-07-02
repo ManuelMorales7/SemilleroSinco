d60b7603eff64dcade17a9ee1dc8a2489d5899dd
josearleck <74557829+josearleck@users.noreply.github.com>
2021-07-02 09:25:30 -0500

    Add files via upload

---
+++ Temario/login.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Login</title>
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap" rel="stylesheet"> 
+	<link rel="stylesheet" href="bootstrap.min.css">
+</head>
+<body>  
+    
+<div class="container">
+          <div class="row">
+            
+            <div class="col-md-4">
+                
+            </div>
+
+              <div class="col-md-4">
+
+                    <br>
+                    <br>
+                    <br>
+
+
+                    <div class="card">
+                        <div class="card-header">
+                            Login
+                        </div>
+                        <div class="card-body">
+
+                            <form method="POST">
+                            
+                            <div class = "form-group">
+                            <label >Usuario</label>
+                            <input type="text" class="form-control" name="usuario"  placeholder="Ingrese su usuario">
+                            </div>
+
+                            <div class="form-group">x|
+                            <label >Contraseña:</label>
+                            <input type="password" class="form-control" name="contraseña" placeholder="Ingrese su contraseña">
+                            </div>
+
+                            
+                            <button type="submit" class="btn btn-primary">Entrar</button>
+                            </form>
+                        
+                        
+                                                                        
+                        </div>
+                    </div>
+                  
+              </div>
+              
+          </div>
+      </div>
+    
+</body>
+</html>
