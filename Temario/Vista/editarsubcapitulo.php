54390a6dcd94866142c88481870a6fa292210642
Inatys <54780848+Inatys@users.noreply.github.com>
2021-07-07 17:51:23 -0500

    Add files via upload
    Agregue las vistas de eliminar, editar y agregar capítulos y subcapítulos

---
+++ Temario/Vista/editarsubcapitulo.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     
+     <title>Editar Capitulo</title>
+     <link rel="stylesheet" href="css/default.css">
+   </head>
+   <body>
+     <center>
+
+       <div class="agregacion">
+         <form id="formulario" action="index.html" method="post">
+         <br><br>
+           <h3 class="title">Edicion de subcapitulo</h3>
+           <br><br>
+           <p style="">numero subcapitulo</p>
+           <input type="text" id="numero_subcapitulo"  title="Se necesita nombre del producto" required>
+           <br><br>
+           <p style="">titulo subcapitulo</p>
+           <input type="text" id="titulo_subcapitulo"  title="Se necesita nombre del producto" required>
+           <br><br>    
+           <div class="">
+               <p id="mensajee" style="color: red"></p>
+              <p id="mensaje" style="color: #44EC1E"></p>
+
+           </div>
+           <br><br><br>
+           <button type="button" name="button" id="editar" class="uno">Editar capitulo</button>
+
+         </form>
+
+       </div>
+     </center>
+     <script src="js/jqueryy.js" charset="utf-8"></script>
+     <script src="js/operaciones.js" charset="utf-8"></script>
+   </body>
+ </html>
---
+++ Temario/Vista/eliminarcapitulo.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     
+     <title>Eliminar Capitulo</title>
+     <link rel="stylesheet" href="css/default.css">
+   </head>
+   <body>
+     <center>
+
+       <div class="agregacion">
+         <form id="formulario" action="index.html" method="post">
+         <br><br>
+           <h3 class="title">Eliminacion de capitulo</h3>
+           <br><br>
+           <p style="">numero capitulo</p>
+           <input type="text" id="numero_capitulo"  title="Se necesita nombre del producto" required>
+           <br><br>    
+           <div class="">
+               <p id="mensajee" style="color: red"></p>
+              <p id="mensaje" style="color: #44EC1E"></p>
+
+           </div>
+           <br><br><br>
+           <button type="button" name="button" id="elimina" class="uno">Eliminar capitulo</button>
+
+         </form>
+
+       </div>
+     </center>
+     <script src="js/jqueryy.js" charset="utf-8"></script>
+     <script src="js/operaciones.js" charset="utf-8"></script>
+   </body>
+ </html>
