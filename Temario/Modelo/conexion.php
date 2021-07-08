a3c272772dc3373a1274dd58eef0b0fa5d1b11b7
Manuel Eduardo Morales Perez <43793760+ManuelMorales7@users.noreply.github.com>
2021-07-08 18:00:41 -0500

    Update conexion.php

--- Temario/Modelo/conexion.php
+++ Temario/Modelo/conexion.php
@@ -1,3 +1,4 @@
+
 <?php
 
 class conexion{
@@ -51,7 +52,7 @@ class conexion{
     function registrar_usuario($nombre, $apellido, $usuario, $password){
   
         session_start();
-        $res = $this->conexion->query("select nombre, usuario from usuarios where usuario = '.$usuario.' ");
+        $res = $this->conexion->query("select nombre, usuario from usuarios where usuario = '$usuario' ");
         
             if(mysqli_num_rows($res)>0){
 
@@ -67,105 +68,10 @@ class conexion{
             }
     }
     
-    
-    //funcion agregar capitulo
-public function agregarCapitulo($numero_capitulo, $titulo_capitulo){
-
-        session_start();
-        $res = $this->conexion->query("select numero_capitulo from capitulo where numero_capitulo = '".$numero_capitulo."' ");
-
-
-        if (mysqli_num_rows($res)>0) {
-          echo '1';
-        }else {
-          $this->conexion->query("INSERT INTO capitulo (titulo_capitulo, numero_capitulo) VALUES ('"  .$titulo_capitulo. "'," . $numero_capitulo. ")");
-          echo "¡Se agrego el capitulo!";
-        }
-
-      }
-
-//funcion eliminar capitulo
-      public function eliminarCapitulo($numero_capitulo){
-
-        session_start();
-        $res = $this->conexion->query("select numero_capitulo from capitulo where numero_capitulo = '".$numero_capitulo."' ");
-
-
-        if (mysqli_num_rows($res)>0) {
-          $this->conexion->query("DELETE FROM capitulo WHERE numero_capitulo = '".$numero_capitulo."'");
-          echo "¡Se elimino el capitulo!";
-        }else {         
-          echo '1';
-        }
-
-      }
-
-//funcion editar capitulo
-public function editarCapitulo($numero_capitulo, $titulo_capitulo){
-
-  session_start();
-  $res = $this->conexion->query("select numero_capitulo from capitulo where numero_capitulo = '".$numero_capitulo."' ");
-
-
-  if (mysqli_num_rows($res)>0) {
-    $this->conexion->query("update capitulo set titulo_capitulo = '".$titulo_capitulo."' where numero_capitulo ='".$numero_capitulo."' ");
-    echo "¡Se edito el titulo del capitulo!";
-  }else {         
-    echo '1';
-  }
-
-}
-
-//funcion agregar subcapitulo
-public function agregarSubcapitulo($numero_subcapitulo, $titulo_subcapitulo, $numero_capitulo ){
-
-        session_start();
-        $res = $this->conexion->query("select numero_subcapitulo from temario where numero_subcapitulo = '".$numero_subcapitulo."' ");
-
-
-        if (mysqli_num_rows($res)>0) {
-          echo '1';
-        }else {
-          $this->conexion->query("INSERT INTO temario (numero_subcapitulo, titulo_subcapitulo, numero_capitulo) VALUES ("  .$numero_subcapitulo. ",'" . $titulo_subcapitulo."'," .$numero_capitulo.")");
-          echo "¡Se agrego el subcapitulo!";
-        }
-
-      }
-
-//funcion eliminar subcapitulo
-      public function eliminarSubcapitulo($numero_subcapitulo){
-
-        session_start();
-        $res = $this->conexion->query("select numero_subcapitulo from temario where numero_subcapitulo = '".$numero_subcapitulo."' ");
-
-
-        if (mysqli_num_rows($res)>0) {
-          $this->conexion->query("DELETE FROM temario WHERE numero_subcapitulo = '".$numero_subcapitulo."'");
-          echo "¡Se elimino el subcapitulo!";
-        }else {         
-          echo '1';
-        }
-
-      }
-
-//funcion editar subcapitulo
-public function editarSubcapitulo($numero_subcapitulo, $titulo_subcapitulo){
-
-  session_start();
-  $res = $this->conexion->query("select numero_subcapitulo from temario where numero_subcapitulo = '".$numero_subcapitulo."' ");
-
-
-  if (mysqli_num_rows($res)>0) {
-    $this->conexion->query("update temario set titulo_subcapitulo = '".$titulo_subcapitulo."' where numero_subcapitulo ='".$numero_subcapitulo."' ");
-    echo "¡Se edito el titulo del subcapitulo!";
-  }else {         
-    echo '1';
-  }
-
-}
-
-    
 }
 
 
 ?>
+
+
+
