dee9f36b84d7d3b76e1593e1ea1f14c2c2237012
Manuel Eduardo Morales Perez <43793760+ManuelMorales7@users.noreply.github.com>
2021-07-08 21:33:28 -0500

    Update admin.php

--- Temario/Vista/admin.php
+++ Temario/Vista/admin.php
@@ -1,14 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<?php
+session_start();
+if($_SESSION['validar'] == 1){
 
-    <h1>Bienvenido administrador</h1>
-    
-</body>
-</html>
+?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Admistrador</title>
+    </head>
+    <body>
+
+        <h1>Bienvenido administrador</h1>
+        <a href="permission.php">Agregar Permisos</a>
+        <br>
+        <a href="index.php">¿Cerrar Sesión?</a>
+        
+    </body>
+    </html>
+<?php
+
+}else{
+
+echo "login.php";
+
+}
+?>
